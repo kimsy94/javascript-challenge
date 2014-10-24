0fb2c9f889da56b9ddbb56c80d475db66f42ee13
Sunyoon Kim <kimsy94@uw.edu>
2014-10-24 05:03:05 -0700

    10/24/2014

--- js/signup.js
+++ js/signup.js
@@ -27,18 +27,75 @@ function onReady() {
 }
 
 function checkOccupation(occupation) {
-	console.log(occupation.value);
+	//console.log(occupation.value);
 
 	var occupationOther = document.getElementById('occupationOther');
-	if(occupation.value == 'other') {
+	if (occupation.value == 'other') {
 		occupationOther.style.display = 'block';
 	} else {
 		occupationOther.style.display = 'none';
 	}
 }
 
+
+
+
+
+
 function onSubmit() {
-	
+	var txt;
+	var location;
+
+	var r = confirm("Do you really want to leave?");
+	if (r == true) {
+		location.href = 'http://google.com';
+	} else {
+	    txt = "Cancel";
+	}
 }
+
+
+function validateForm(form) {
+    var requiredFields = ['firstName', 'lastName', 'address1', 'city', 'state', 'zip', 'birthdate'];
+    var idx;
+    var valid = true;
+
+    for (idx = 0; idx < requiredFields.length; ++idx) {
+        valid &= validateRequiredField(form.elements[requiredFields[idx]]);
+    }
+
+    return valid;
+
+} 
+
+/*		if (occupation.value == 'other') {
+		occupationOther.style.display = 'block';
+		} else {
+		occupationOther.style.display = 'none';
+		} */
+
+
+function validateRequiredField(field) {
+    var value = field.value;
+    value = value.trim();
+    var valid = value.length > 0;
+
+    if (valid) {
+        field.className = 'form-control';
+    }
+    else {
+        field.className = 'form-control invalid-field';
+
+        //error-message
+    }
+    return valid;
+
+    var d = new Date(year, month, day);
+    
+    var birthdate = document.getElementById("birthdateMessage")
+    birthdate.innerHTML = "";
+
+
+
 
 document.addEventListener('DOMContentLoaded', onReady);
