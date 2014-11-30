e82df088fff377ea33230bca4ab51c6815828cad
Sunyoon Kim <kimsunyoon@gmail.com>
2014-11-29 17:01:28 -0800

    11/29/2014

--- js/signup.js
+++ js/signup.js
@@ -54,16 +54,6 @@ function onSubmit(evt) {
     try {
         var valid = validateForm(this);
 
-        /*
-        if (!valid) {
-            
-            var errMsg = document.getElementById('error-message');
-            errMsg.innerHTML = 'Please provide values for the required fields!';
-            errMsg.style.display = 'block';
-
-        }
-        */
-
         if (!valid && evt.preventDefault) {
             evt.preventDefault();
         }
@@ -76,21 +66,48 @@ function onSubmit(evt) {
 }
 
 function validateForm(form) {
-    var requiredFields = ['firstName', 'lastName', 'address1', 'city', 'state', 'zip', 'birthdate'];
+    var requiredFields = ['firstName', 'lastName', 'address1', 'city', 'state', 'zip', 'birthdate', 'occupationOther'];
     var idx;
     var valid = true;
 
     for (idx = 0; idx < requiredFields.length; ++idx) {
         valid &= validateRequiredField(form.elements[requiredFields[idx]]);
     }
-
+    var zipCodeField = form.elements['zip']; //validate zip code field
+    var zipRegExp = new RegExp('^\\d{5}$');
+    if(!zipRegExp.test(zipField.value)) {
+        valid = false;
+        zipField.className = 'form-control invalid-form';
+    }
+    var birthday = form.elements['birthdate']; //validates birthdate
+    var birthdayError = document.getElementById('birthdateMessage');
+    var age = calculateAge(birthday.value);
+    if(age < 13) { //checks if user is greater than 13 by calculated age
+        valid = false;
+        birthday.className = 'form-control invalid-form';
+        birthdayError.style.display = 'block';
+    }
+    else {
+        birthdayError.style.display = 'none';
+    }
     return valid;
-} 
+}
 
 function validateRequiredField(field) {
     var value = field.value;
     value = value.trim();
     var valid = value.length > 0;
+    var zipRegExp = new RegExp('^\\d{5}$');
+
+    if (field.name == 'zip') { //checks field if zip is 5 num.
+        valid &= zipRegExp.test(value);
+    }
+    if (field.name == 'birthdate') { //sees if birthdate is a num.
+        var test = value.replace(/\//g,''); //removes slashes
+        test = !isNaN(test);
+        valid &= test;
+        console.log(test);
+    }
 
     if (valid) {
         field.className = 'form-control';
@@ -101,33 +118,17 @@ function validateRequiredField(field) {
     return valid;
 }
 
-/* function zipCode() {
-    var zipRegExp = new RegExp('^\\d{5}$');
-    var patt = /w3schools/i;
-    var patt = new RegExp("e");
+function calculateAge(birthDate) { //calculates the age of user using birth date
+    birthDate = new Date(birthDate);
+    var today = new Date();
+    var yearsDiff = today.getFullYear() - birthDate.getUTCFullYear(); //uses current date to calc. age
+    var monthsDiff = today.getMonth() - dob.getUTCMonth();
+    var daysDiff = today.getDate() - dob.getUTCDate();
 
-    if (zipRegExp.test(input.value)) {
-        evt.preventDefault();
-        evt.returnValue = false;
-    } 
-    return ; 
-} */
-
-
-
-/* function {
-    if (occupation.value == 'other') {
-    occupationOther.style.display = 'block';
-    } else {
-    occupationOther.style.display = 'none';
-    } 
-
-}    
-
-
-    var d = new Date(year, month, day);
-    
-    var birthdate = document.getElementById("birthdateMessage")
-    birthdate.innerHTML = ""; */
+    if (monthsDiff < 0 || (0 == monthsDiff && daysDiff < 0)) {
+        yearsDiff--;
+    }
+    return yearsDiff;
+}
 
 document.addEventListener('DOMContentLoaded', onReady);
